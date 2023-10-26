9bab2a9969bfc3e651d41e7a7ca17762b22f5751
nicole <ngrapai0@gmail.com>
2023-10-25 22:42:14 -0400

    committing pr

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,14 +1,25 @@
 # TODO: Feature 1
-from app import app 
+from app import app, movie_repository
 
 
 def test_get_all_movie_page(test_app):
-    test_app = app.test_client()
     response = test_app.get('/movies')
 
     data = response.data.decode('utf-8')
     
     assert response.status_code == 200
     assert "There Are No Movies To Display!" in data
-    # assert "All Movies" in data
-    # assert "See Our List of Movie Ratings Below" in data
+
+def test_get_movies(test_app):
+    movie_repository.create_movie('Jurassic Park', "Steven Spielberg", "5")
+    
+    response = test_app.get('/movies')
+    data = response.data.decode('utf-8')
+
+    assert response.status_code == 200
+    assert "All Movies" in data
+    assert "See Our List of Movie Ratings Below" in data
+    assert "Jurassic Park" in data
+    assert "Steven Spielberg" in data
+    assert "5" in data
+    
