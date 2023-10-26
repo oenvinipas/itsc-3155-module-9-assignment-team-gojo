2dbefe094fa7022bf59f1b88d04bda66f93e76ab
nicole <ngrapai0@gmail.com>
2023-10-25 20:47:19 -0400

    committing test to merge

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,18 @@
 # TODO: Feature 1
+from app import movie_repository
+from src.repositories.movie_repository import get_movie_repository
+
+# check if get_all_movies() returns a dictionary that is empty/no movies are in the 
+# repository.
+def test_get_all_movies():
+    movie_repo = get_movie_repository()
+    movie_repo.clear_db()
+    assert len(movie_repo.get_all_movies()) == 0
+    
+def test_get_one_movie():
+    movie_repo = get_movie_repository()
+    movie_repo.clear_db()
+
+    movie_repository.create_movie('Jurassic Park', "Steven Spielberg", "5")
+    movie_repository.create_movie('Whiplash', 'Damien Chazelle', '4')
+    assert len(movie_repo.get_all_movies()) == 2
